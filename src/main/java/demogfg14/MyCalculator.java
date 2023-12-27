4ef4b9d94659625bae57039b81362bc61542b75c
Shubham Kumar <ershubhamkumar06@gmail.com>
2023-12-27 11:31:33 +0530

    first commit

---
+++ src/main/java/demogfg14/MyCalculator.java
@@ -0,0 +1,17 @@
+package demogfg14;
+
+public class MyCalculator {
+	 public int sum(int a,int b){
+	        return  a+b;
+	    }
+	    public int diff(int a ,int b){
+	        return a-b;
+	    }
+	    public static void main(String[] args){
+	        MyCalculator cal=new MyCalculator();
+	        System.out.println("sum is:"+cal.sum(20,10));
+	        System.out.println("diff is:"+cal.diff(20,10));
+	    }
+	}
+
+
