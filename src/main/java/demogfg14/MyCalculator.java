2f90d121fdd465874f40e448ded975a652b4b487
Shubham Kumar <ershubhamkumar06@gmail.com>
2023-12-27 12:42:21 +0530

    div feature

--- src/main/java/demogfg14/MyCalculator.java
+++ src/main/java/demogfg14/MyCalculator.java
@@ -7,10 +7,14 @@ public class MyCalculator {
 	    public int diff(int a ,int b){
 	        return a-b;
 	    }
+	    public int div(int a ,int b){
+	        return a/b;
+	    }
 	    public static void main(String[] args){
 	        MyCalculator cal=new MyCalculator();
 	        System.out.println("sum is:"+cal.sum(20,10));
 	        System.out.println("diff is:"+cal.diff(20,10));
+	        System.out.println("div is:"+cal.div(20,10));
 	    }
 	}
 
