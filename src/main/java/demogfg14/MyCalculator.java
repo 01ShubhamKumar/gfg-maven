9cd4964f65968a0cc703c3e06245421fe5f6903d
Shubham Kumar <ershubhamkumar06@gmail.com>
2023-12-27 15:34:32 +0530

    mul added

--- src/main/java/demogfg14/MyCalculator.java
+++ src/main/java/demogfg14/MyCalculator.java
@@ -10,11 +10,15 @@ public class MyCalculator {
 	    public int div(int a ,int b){
 	        return a/b;
 	    }
+	    public int mul(int a ,int b){
+	        return a*b;
+	    }
 	    public static void main(String[] args){
 	        MyCalculator cal=new MyCalculator();
 	        System.out.println("sum is:"+cal.sum(20,10));
 	        System.out.println("diff is:"+cal.diff(20,10));
 	        System.out.println("div is:"+cal.div(20,10));
+	        System.out.println("mul is:"+cal.mul(20,10));
 	    }
 	}
 
